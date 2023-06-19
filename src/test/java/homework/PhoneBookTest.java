c6689fbfd8f844a43e5b118e128d7e540fb1496f
Nikita Novikov <monstars51@gmail.com>
2023-06-19 12:50:49 +0300

    Stub and tests for add

--- src/test/java/homework/PhoneBookTest.java
+++ src/test/java/homework/PhoneBookTest.java
@@ -1,5 +1,41 @@
 package homework;
 
-public class PhoneBookTest {
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PhoneBookTest {
+    private static PhoneBook phoneBook = new PhoneBook();
+    private static int testNumber;
+    @BeforeAll
+    public static void startTest() {
+        System.out.println("Running Tests...");
+        testNumber = 0;
+    }
+
+    @BeforeEach
+    public void iniTest() {
+        testNumber++;
+        System.out.println("Starting test №: " + testNumber);
+    }
+
+
+    @Test
+    public void addContactReturnQuantity() {
+        final int expected = 1;
+        final int actual = phoneBook.add("One", "111-111");
+        assertThat(actual, greaterThanOrEqualTo(expected));
+    }
+
+    @Test
+    public void addRepeatedContact (){
+        final int expected = 1;
+        phoneBook.add("One", "111-111");
+        final int actual = phoneBook.add("One", "222-222");
+        assertEquals(expected, actual);
+    }
 }
